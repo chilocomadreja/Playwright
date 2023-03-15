a0667a102d4ba723c636301c1e82711ec6ae09fb
Sebastian <sebastian.marzec@poczta.fm>
2023-03-15 23:25:44 +0100

    po instalce prettiera

--- tests/SetupUsersCheck.spec.ts
+++ tests/SetupUsersCheck.spec.ts
@@ -1,23 +1,23 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("Setup/Users check", async ({ page }) => {
-  await page.goto("https://spar--preprod.sandbox.my.salesforce.com/");
-  await page.getByLabel("Username").click();
-  await page.getByLabel("Username").fill("admin-bvrm@force.com.preprod");
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").fill("Clorce10@");
-  await page.getByRole("button", { name: "Log In to Sandbox" }).click();
+test('Setup/Users check', async ({ page }) => {
+  await page.goto('https://spar--preprod.sandbox.my.salesforce.com/');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('admin-bvrm@force.com.preprod');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Clorce10@');
+  await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
   await page
-    .getByRole("button", { name: "Setup" })
-    .getByRole("button", { name: "Setup", exact: true })
-    .filter({ hasText: "Setup" })
+    .getByRole('button', { name: 'Setup' })
+    .getByRole('button', { name: 'Setup', exact: true })
+    .filter({ hasText: 'Setup' })
     .click();
-  const page1Promise = page.waitForEvent("popup");
+  const page1Promise = page.waitForEvent('popup');
   await page
-    .getByRole("menuitem", {
-      name: "Setup Opens in a new tab Setup for current app",
+    .getByRole('menuitem', {
+      name: 'Setup Opens in a new tab Setup for current app',
     })
     .click();
   const page1 = await page1Promise;
-  await page1.getByRole("link", { name: "Users" }).click();
+  await page1.getByRole('link', { name: 'Users' }).click();
 });
--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -1,25 +1,25 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("Login page", async ({ page }) => {
-  await page.goto("https://spar--preprod.sandbox.my.salesforce.com/");
-  await page.getByLabel("Username").click();
-  await page.getByLabel("Username").fill("ekspansjaautotest@clorce.preprod.pl");
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").fill("Clorce4@");
-  await page.getByRole("button", { name: "Log In to Sandbox" }).click();
+test('Login page', async ({ page }) => {
+  await page.goto('https://spar--preprod.sandbox.my.salesforce.com/');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('ekspansjaautotest@clorce.preprod.pl');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Clorce4@');
+  await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
   await page.goto(
-    "https://spar--preprod.sandbox.lightning.force.com/lightning/page/home"
+    'https://spar--preprod.sandbox.lightning.force.com/lightning/page/home'
   );
 });
 
-test("login page2", async ({ page }) => {
-  await page.goto("https://spar--preprod.sandbox.my.salesforce.com/");
-  await page.getByLabel("Username").click();
-  await page.getByLabel("Username").fill("ekspansjaautotest@clorce.preprod.pl");
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").fill("Clorce4@");
-  await page.getByRole("button", { name: "Log In to Sandbox" }).click();
+test('login page2', async ({ page }) => {
+  await page.goto('https://spar--preprod.sandbox.my.salesforce.com/');
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill('ekspansjaautotest@clorce.preprod.pl');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Clorce4@');
+  await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
   await page.goto(
-    "https://spar--preprod.sandbox.lightning.force.com/lightning/page/home"
+    'https://spar--preprod.sandbox.lightning.force.com/lightning/page/home'
   );
 });
