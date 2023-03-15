c7351a0b84a2ad889d0d5d9e1915becc19d29a62
Sebastian <sebastian.marzec@poczta.fm>
2023-03-15 23:20:21 +0100

    added prettier

--- tests/LeadTab.spec.ts
+++ tests/LeadTab.spec.ts
@@ -1,15 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Lead tab', async ({ page }) => {
-  await page.goto('https://spar--preprod.sandbox.my.salesforce.com/');
-  await page.getByLabel('Username').click();
-  await page.getByLabel('Username').fill('ekspansjaautotest@clorce.preprod.pl');
-  await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('Clorce4@');
-  await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
-  await page.getByRole('link', { name: 'Potencjalni klienci' }).click();
-  await page.locator('one-appnav').click();
- 
- 
-
- });
+test("Lead tab", async ({ page }) => {
+  await page.goto("https://spar--preprod.sandbox.my.salesforce.com/");
+  await page.getByLabel("Username").click();
+  await page.getByLabel("Username").fill("ekspansjaautotest@clorce.preprod.pl");
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill("Clorce4@");
+  await page.getByRole("button", { name: "Log In to Sandbox" }).click();
+  await page.getByRole("link", { name: "Potencjalni klienci" }).click();
+  await page.locator("one-appnav").click();
+});
