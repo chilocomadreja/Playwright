4f6e026f20399c4175fa4366ee8a1fb1933c8f7a
Sebastian <sebastian.marzec@poczta.fm>
2023-04-14 12:55:19 +0200

    zmiana nazwy testu i zmiana w configu czasu

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.parallel('Login / logout Flow', () => {
+test.describe.parallel('Login to sandbox', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://test.salesforce.com/');
   });
