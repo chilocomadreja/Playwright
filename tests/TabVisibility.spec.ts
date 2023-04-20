fbb8d7ed10b209c766cf24b527b2325d2d12631d
chilocomadreja <sebastian.marzec@poczta.fm>
2023-04-20 23:20:35 +0200

    poprawione testy dotyczące Tabs

--- tests/TabVisibility.spec.ts
+++ tests/TabVisibility.spec.ts
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Tabs', () => {
+test.describe.parallel('Tabs', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://test.salesforce.com/')
+  
+  });
+
   test('Tabs visibility', async ({ page }) => {
     //Arrange
     const username = 'admin-bvrm@force.com.sebamaboxa';
@@ -8,13 +13,12 @@ test.describe('Tabs', () => {
 
     ///Act
 
-    await page.goto('https://test.salesforce.com/');
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
     await page.getByLabel('Password').fill(password);
     await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
-    await page.getByRole('link', { name: 'Konta' }).click();
+    await page.click('text=Konta');
     await page.locator('one-appnav').click();
 
     ///Assert
@@ -25,7 +29,7 @@ test.describe('Tabs', () => {
     const username = 'admin-bvrm@force.com.sebamaboxa';
     const password = 'Clorce9@';
     ///Act
-    await page.goto('https://test.salesforce.com/');
+
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
@@ -39,8 +43,9 @@ test.describe('Tabs', () => {
     //Arrange
     const username = 'admin-bvrm@force.com.sebamaboxa';
     const password = 'Clorce9@';
+
     ///Act
-    await page.goto('https://test.salesforce.com/');
+
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
@@ -57,7 +62,6 @@ test.describe('Tabs', () => {
 
     ///Act
 
-    await page.goto('https://test.salesforce.com/');
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
@@ -72,13 +76,13 @@ test.describe('Tabs', () => {
     const username = 'admin-bvrm@force.com.sebamaboxa';
     const password = 'Clorce9@';
     ///Act
-    await page.goto('https://test.salesforce.com/');
+
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
     await page.getByLabel('Password').fill(password);
     await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
-    await page.getByRole('link', { name: 'Zadania' }).click();
+    await page.click('text=Zadania');
     await page.locator('one-appnav').click();
   });
 
@@ -87,7 +91,7 @@ test.describe('Tabs', () => {
     const username = 'admin-bvrm@force.com.sebamaboxa';
     const password = 'Clorce9@';
     ///Act
-    await page.goto('https://test.salesforce.com/');
+
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
@@ -102,7 +106,7 @@ test.describe('Tabs', () => {
     const username = 'admin-bvrm@force.com.sebamaboxa';
     const password = 'Clorce9@';
     ///Act
-    await page.goto('https://test.salesforce.com/');
+
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
@@ -118,45 +122,12 @@ test.describe('Tabs', () => {
     const password = 'Clorce9@';
     ///Act
 
-    await page.goto('https://test.salesforce.com/');
     await page.getByLabel('Username').click();
     await page.getByLabel('Username').fill(username);
     await page.getByLabel('Password').click();
     await page.getByLabel('Password').fill(password);
     await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.getByRole('link', { name: 'Raporty' }).click();
-    await page.locator('one-appnav').click();
-  });
-
-  test('Notes tab visibility', async ({ page }) => {
-    //Arrange
-    const username = 'admin-bvrm@force.com.sebamaboxa';
-    const password = 'Clorce9@';
-
-    ///Act
-    await page.goto('https://test.salesforce.com/');
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill(username);
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill(password);
-    await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
-    await page.getByRole('link', { name: 'Notatki' }).click();
-    await page.locator('one-appnav').click();
-  });
-
-  test('Groups tab visibility', async ({ page }) => {
-    //Arrange
-    const username = 'admin-bvrm@force.com.sebamaboxa';
-    const password = 'Clorce9@';
-
-    ///Act
-    await page.goto('https://test.salesforce.com/');
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill(username);
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill(password);
-    await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
-    await page.getByRole('link', { name: 'Grupy' }).click();
     await page.locator('one-appnav').click();
   });
 });
