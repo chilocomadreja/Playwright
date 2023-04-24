6da097ee98fff4f76fba2394da049f61a7039aae
chilocomadreja <sebastian.marzec@poczta.fm>
2023-04-24 22:29:24 +0200

    refactor code 2

--- tests/NavigationMenuAdministator.spec.ts
+++ tests/NavigationMenuAdministator.spec.ts
@@ -8,22 +8,9 @@ test.describe.parallel('Tabs', () => {
     await page.type('#username', 'administratortest@clorce.com');
     await page.type('#password', 'Clorce1@');
     await page.click('text=Log In to Sandbox');
-
-
   });
 
   test('Tabs visibility', async ({ page }) => {
-    // //Arrange
-    // const username = 'administratortest@clorce.com';
-    // const password = 'Clorce1@';
-
-    // ///Act
-
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill(username);
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill(password);
-    // await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.click('text=Konta');
     await page.locator('one-appnav').click();
 
@@ -31,92 +18,31 @@ test.describe.parallel('Tabs', () => {
   });
 
   test('Contacts tab visibility', async ({ page }) => {
-    // //Arrange
-    // const username = 'administratortest@clorce.com';
-    // const password = 'Clorce1@';
-    // ///Act
-
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill(username);
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill(password);
-    // await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.getByRole('link', { name: 'Kontakty' }).click();
     await page.locator('one-appnav').click();
   });
 
   test('Opportunities tab visibility', async ({ page }) => {
-    // //Arrange
-    // const username = 'administratortest@clorce.com';
-    // const password = 'Clorce1@';
-
-    // ///Act
-
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill(username);
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill(password);
-    // await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.getByRole('link', { name: 'Klienci w procesie' }).click();
     await page.locator('one-appnav').click();
   });
 
   test('Tasks tab visibility', async ({ page }) => {
-    // //Arrange
-    // const username = 'administratortest@clorce.com';
-    // const password = 'Clorce1@';
-    // ///Act
-
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill(username);
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill(password);
-    // await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.click('text=Zadania');
     await page.locator('one-appnav').click();
   });
 
   test('Calendar tab visibility', async ({ page }) => {
-    // //Arrange
-    // const username = 'administratortest@clorce.com';
-    // const password = 'Clorce1@';
-    // ///Act
-
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill(username);
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill(password);
-    // await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.getByRole('link', { name: 'Kalendarz' }).click();
     await page.locator('one-appnav').click();
   });
 
   test('Dashboards tab visibility', async ({ page }) => {
-    // //Arrange
-    // const username = 'administratortest@clorce.com';
-    // const password = 'Clorce1@';
-    // ///Act
-
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill(username);
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill(password);
-    // await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.getByRole('link', { name: 'Pulpity' }).click();
     await page.locator('one-appnav').click();
   });
 
   test('Reports tab visibility', async ({ page }) => {
-    // //Arrange
-    // const username = 'administratortest@clorce.com';
-    // const password = 'Clorce1@';
-    // ///Act
-
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill(username);
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill(password);
-    // await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
     await page.getByRole('link', { name: 'Raporty' }).click();
     await page.locator('one-appnav').click();
   });
