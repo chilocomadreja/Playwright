c575c9f98d41946695b14c1bd0eb06e34604bd45
chilocomadreja <sebastian.marzec@poczta.fm>
2023-05-25 14:40:59 +0200

    zmiana menu i ptypadkow testowych dla ekspansja delete

--- tests/NavigationMenu/NavigationMenuEkspansjaDeleteb2b.spec.ts
+++ tests/NavigationMenu/NavigationMenuEkspansjaDeleteb2b.spec.ts
@@ -6,46 +6,46 @@ test.describe.parallel('Menu: Sprzedaz', () => {
 
     await page.click('text=Log In to Sandbox');
     await page.type('#username', 'ekspansjadeleteb2b@clorce.com.sebamaboxa');
-    await page.type('#password', 'Clorce2@');
+    await page.type('#password', 'Clorce3@');
     await page.click('text=Log In to Sandbox');
   });
 
-  test('Tabs visibility', async ({ page }) => {
+  test('Accounts', async ({ page }) => {
     await page.click('text=Konta');
     await page.locator('one-appnav').click();
   });
 
-  test('Contacts tab visibility', async ({ page }) => {
+  test('Contacts', async ({ page }) => {
     await page.getByRole('link', { name: 'Kontakty' }).click();
     await page.locator('one-appnav').click();
   });
 
-  test('Leads tab visibility', async ({ page }) => {
+  test('Leads', async ({ page }) => {
     await page.getByRole('link', { name: 'Potencjalni klienci' }).click();
     await page.locator('one-appnav').click();
   });
 
-  test('Opportunities tab visibility', async ({ page }) => {
+  test('Opportunities', async ({ page }) => {
     await page.getByRole('link', { name: 'Klienci w procesie' }).click();
     await page.locator('one-appnav').click();
   });
 
-  test('Tasks tab visibility', async ({ page }) => {
+  test('Tasks', async ({ page }) => {
     await page.click('text=Zadania');
     await page.locator('one-appnav').click();
   });
 
-  test('Calendar tab visibility', async ({ page }) => {
+  test('Calendar', async ({ page }) => {
     await page.getByRole('link', { name: 'Kalendarz' }).click();
     await page.locator('one-appnav').click();
   });
 
-  test('Dashboards tab visibility', async ({ page }) => {
+  test('Dashboards', async ({ page }) => {
     await page.getByRole('link', { name: 'Pulpity' }).click();
     await page.locator('one-appnav').click();
   });
 
-  test('Reports tab visibility', async ({ page }) => {
+  test('Reports', async ({ page }) => {
     await page.getByRole('link', { name: 'Raporty' }).click();
     await page.locator('one-appnav').click();
   });
