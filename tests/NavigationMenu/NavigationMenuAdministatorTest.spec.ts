51867d1876f239559ca8a898042622f45b747830
chilocomadreja <sebastian.marzec@poczta.fm>
2023-05-25 11:45:07 +0200

    poprawa pracownik finansowy

--- tests/NavigationMenu/NavigationMenuAdministatorTest.spec.ts
+++ tests/NavigationMenu/NavigationMenuAdministatorTest.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe.parallel.only('Tabs', () => {
+test.describe.parallel('Tabs', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://test.salesforce.com/');
 
--- tests/NavigationMenu/NavigationMenuPracownikFinansowy.spec.ts
+++ tests/NavigationMenu/NavigationMenuPracownikFinansowy.spec.ts
@@ -5,23 +5,16 @@ test.describe.parallel('Tabs', () => {
     await page.goto('https://test.salesforce.com/');
 
     await page.click('text=Log In to Sandbox');
-    await page.type('#username', 'pracownikfinansowy@clorce.com.sebamaboxa');
-    await page.type('#password', 'Clorce1@');
-    await page.click('text=Log In to Sandbox');
+    await page
+      .getByLabel('Username')
+      .fill('pracownikfinansow@clorce.com.sebamaboxa');
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('Clorce1@');
+    await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
   });
 
   test('Accounts visibility', async ({ page }) => {
     await page.getByRole('link', { name: 'Konta', exact: true }).click();
-    await page.locator('one-appnav').click();
-  });
-
-  test('Invoices tab visibility', async ({ page }) => {
-    await page.getByRole('link', { name: 'Faktury' }).click();
-    await page.locator('one-appnav').click();
-  });
-
-  test('Import faktur tab visibility', async ({ page }) => {
-    await page.getByRole('link', { name: 'Import faktur' }).click();
     await page.locator('one-appnav').click();
   });
 
@@ -30,13 +23,23 @@ test.describe.parallel('Tabs', () => {
     await page.locator('one-appnav').click();
   });
 
-  test('Purchase Orders tab visibility', async ({ page }) => {
-    await page.getByRole('link', { name: 'Zamówienia SPAR' }).click();
+  test('Opportunities tab visibility', async ({ page }) => {
+    await page.getByRole('link', { name: 'Klienci w procesie' }).click();
     await page.locator('one-appnav').click();
   });
 
-  test('Approval Requests tab visibility', async ({ page }) => {
-    await page.getByRole('link', { name: 'Prośby o zatwierdzenie' }).click();
+  test('Tasks tab visibility', async ({ page }) => {
+    await page.click('text=Zadania');
+    await page.locator('one-appnav').click();
+  });
+
+  test('Calendar tab visibility', async ({ page }) => {
+    await page.getByRole('link', { name: 'Kalendarz' }).click();
+    await page.locator('one-appnav').click();
+  });
+
+  test('Dashboards tab visibility', async ({ page }) => {
+    await page.getByRole('link', { name: 'Pulpity' }).click();
     await page.locator('one-appnav').click();
   });
 
