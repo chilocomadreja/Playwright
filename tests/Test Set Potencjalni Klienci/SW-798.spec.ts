30dc2d4ea376e64f9b217d9db52311db632ee94e
chilocomadreja <sebastian.marzec@poczta.fm>
2023-05-30 00:22:21 +0200

    sw-798 zrobione

---
+++ tests/Test Set Potencjalni Klienci/SW-798.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('weryfikacja PS - Data entry basic user - tworzenie leada', async ({
+  page,
+}) => {
+  const url = 'https://test.salesforce.com/';
+  const login = 'ekspansjabasic@clorce.com.sebamaboxa';
+  const password = 'Clorce1@';
+
+  await page.goto(url);
+  await page.getByLabel('Username').click();
+  await page.getByLabel('Username').fill(login);
+  await page.getByLabel('Password').fill(password);
+  await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
+  await page.click('text=Potencjalni klienci');
+  await page.click('text=Nowy element');
+  await page.getByRole('button', { name: 'Zapisz', exact: true }).click();
+  await page.locator('records-modal-lwc-detail-panel-wrapper').click();
+
+  const komunikat = 'Napotkaliśmy nieoczekiwaną przeszkodę.';
+  expect(komunikat).toContain('Napotkaliśmy nieoczekiwaną przeszkodę.');
+});
