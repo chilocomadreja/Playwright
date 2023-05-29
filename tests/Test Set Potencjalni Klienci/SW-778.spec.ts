cf1d5e883ac8bf337d005b87801c6b4d487ac53f
chilocomadreja <sebastian.marzec@poczta.fm>
2023-05-29 23:45:49 +0200

    format document dla sw-778

--- tests/Test Set Potencjalni Klienci/SW-778.spec.ts
+++ tests/Test Set Potencjalni Klienci/SW-778.spec.ts
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 test.only('weryfikacja PS - Delete records- mozliwosc usuniecia leada', async ({
   page,
 }) => {
-
   const url = 'https://test.salesforce.com/';
   const login = 'ekspansjadeleteb2b@clorce.com.sebamaboxa';
   const password = 'Clorce3@';
@@ -17,5 +16,5 @@ test.only('weryfikacja PS - Delete records- mozliwosc usuniecia leada', async ({
   ///nazwa leada musi być zawsze taka sama. Lead musi być wcześniej utworzony!!!!
   await page.click('text=Margarita Lenowo');
   await page.click('text=Usuń');
- await page.click('//span[contains(text(),\'Usuń\')]')
+  await page.click("//span[contains(text(),'Usuń')]");
 });
