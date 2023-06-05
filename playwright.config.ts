6486c9d11c88379dfe4fb159931ae9aeb76ce20b
chilocomadreja <sebastian.marzec@poczta.fm>
2023-06-05 17:11:15 +0200

    flaky test dodane oraz wielkość

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+
   testDir: './tests',
+  
   /* Maximum time one test can run for. */
   timeout: 80 * 1000,
   expect: {
@@ -20,12 +22,14 @@ export default defineConfig({
      */
     timeout: 80000,
   },
+ 
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -88,4 +92,5 @@ export default defineConfig({
   //   command: 'npm run start',
   //   port: 3000,
   // },
+ 
 });
--- tests/NavigationMenu/NavigationMenu.config.ts
+++ tests/NavigationMenu/NavigationMenu.config.ts
@@ -25,7 +25,8 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -46,7 +47,11 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], 
+    
+      viewport:{width:1926,height:955}
+    },
+      
     },
 
     // {
