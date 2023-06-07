f94c3f7b3ae6b02afc9ccf9b95e5543d90f5732e
chilocomadreja <sebastian.marzec@poczta.fm>
2023-06-07 17:06:10 +0200

    format kodu

--- tests/Test Set Tworzenie i procesowanie Potencjalnego Klienta/SW-838.spec.ts
+++ tests/Test Set Tworzenie i procesowanie Potencjalnego Klienta/SW-838.spec.ts
@@ -4,62 +4,53 @@ test.describe.parallel('CRM- Stworzenie Potencjalnego Klienta', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://test.salesforce.com/');
 
-  await page.click('text=Log In to Sandbox');
+    await page.click('text=Log In to Sandbox');
     await page.type('#username', 'ekspansjanormal@clorce.com.sebamaboxa');
     await page.type('#password', 'Clorce2@');
     await page.click('text=Log In to Sandbox');
-  
 
+    test('błędny nr tel. komórkowy', async ({ page }) => {
+      await page.click('text=Potencjalni klienci');
 
- test('błędny nr tel. komórkowy', async ({ page }) => { 
+      await page.click('text=Nowy element');
+      ///Informacje o potencjalnym kliencie
 
-  await page.click('text=Potencjalni klienci');
+      await page.getByLabel('Numer projektu').fill('777');
+      await page.getByPlaceholder('Nazwisko').click();
+      await page.getByPlaceholder('Nazwisko').fill('lastName');
+      await page.getByLabel('*Firma').fill('companyName');
+      await page.getByRole('textbox', { name: 'Tel. komórkowy' }).fill('test');
 
-  await page.click('text=Nowy element');
-  ///Informacje o potencjalnym kliencie
+      const komunikat4 =
+        'Pole tel.komórkowy może zawierać tylko cyfry lub powinno pozostać puste!Sprawdź ponownie te pole, a następnie wpisz właściwe dane.';
+      expect(komunikat4).toContain(
+        'Pole tel.komórkowy może zawierać tylko cyfry lub powinno pozostać puste!Sprawdź ponownie te pole, a następnie wpisz właściwe dane.'
+      );
+    });
 
-  await page.getByLabel('Numer projektu').fill('777');
-  await page.getByPlaceholder('Nazwisko').click();
-  await page.getByPlaceholder('Nazwisko').fill('lastName');
-  await page.getByLabel('*Firma').fill('companyName');
-  await page
-    .getByRole('textbox', { name: 'Tel. komórkowy' })
-    .fill('test');
+    test('błędny adres mailowy', async ({ page }) => {
+      await page.click('text=Potencjalni klienci');
 
+      await page.click('text=Nowy element');
+      ///Informacje o potencjalnym kliencie
 
-
-
-    const komunikat4 = 'Pole tel.komórkowy może zawierać tylko cyfry lub powinno pozostać puste!Sprawdź ponownie te pole, a następnie wpisz właściwe dane.';
-    expect(komunikat4).toContain('Pole tel.komórkowy może zawierać tylko cyfry lub powinno pozostać puste!Sprawdź ponownie te pole, a następnie wpisz właściwe dane.');
-
-});
-
-test('błędny adres mailowy', async ({ page }) => {
-    await page.click('text=Potencjalni klienci');
-
-    await page.click('text=Nowy element');
-    ///Informacje o potencjalnym kliencie
-  
-    await page.getByLabel('Numer projektu').fill('777');
-    await page.getByPlaceholder('Nazwisko').click();
-    await page.getByPlaceholder('Nazwisko').fill('lastName');
-    await page.getByLabel('*Firma').fill('companyName');
-    await page
-      .getByRole('textbox', { name: 'Tel. komórkowy' })
-      .fill('666777888');
+      await page.getByLabel('Numer projektu').fill('777');
+      await page.getByPlaceholder('Nazwisko').click();
+      await page.getByPlaceholder('Nazwisko').fill('lastName');
+      await page.getByLabel('*Firma').fill('companyName');
+      await page
+        .getByRole('textbox', { name: 'Tel. komórkowy' })
+        .fill('666777888');
       await page.getByRole('textbox', { name: 'E-mail' }).fill('1234');
-
 
       const komunikat5 = 'Napotkaliśmy nieoczekiwaną przeszkodę.';
       expect(komunikat5).toContain('Napotkaliśmy nieoczekiwaną przeszkodę.');
 
-      const komunikat6 = 'Napotkaliśmy nieoczekiwaną przeszkodę. Sprawdź następujące pola';
-      expect(komunikat6).toContain('Napotkaliśmy nieoczekiwaną przeszkodę. Sprawdź następujące pola');
-
-
-
-
+      const komunikat6 =
+        'Napotkaliśmy nieoczekiwaną przeszkodę. Sprawdź następujące pola';
+      expect(komunikat6).toContain(
+        'Napotkaliśmy nieoczekiwaną przeszkodę. Sprawdź następujące pola'
+      );
     });
-
-});
+  });
 });
