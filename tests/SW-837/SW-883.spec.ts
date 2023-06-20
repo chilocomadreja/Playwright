84dd90527f627b62b6b119baeed090297d1fec9f
chilocomadreja <sebastian.marzec@poczta.fm>
2023-06-20 11:15:45 +0200

    Sw-883 gotowe całe

---
+++ tests/SW-837/SW-883.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.parallel('Widoczność zakłądek na POtencjalnym Kliencie', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://test.salesforce.com/');
+    await page.click('text=Log In to Sandbox');
+    await page.type('#username', 'ekspansjanormal@clorce.com.sebamaboxa');
+    await page.type('#password', 'Clorce2@');
+    await page.click('text=Log In to Sandbox');
+  });
+
+  test('Widok zakładek', async ({ page }) => {
+    await page.getByRole('link', { name: 'Potencjalni klienci' }).click();
+    await page.waitForTimeout(5000);
+    await page.getByRole('link', { name: 'Janusz Lech' }).click();
+    await page.waitForTimeout(5000);
+  });
+
+  const Czynność = 'Czynność';
+  expect(Czynność).toContain('Czynność');
+
+  const Szczegóły = 'Szczegóły';
+  expect(Szczegóły).toContain('Szczegóły');
+
+  const Chatter = 'Chatter';
+  expect(Chatter).toContain('Chatter');
+
+  const ZadaniaZZ = 'Zadanie z załącznikiem';
+  expect(ZadaniaZZ).toContain('Zadanie z załącznikiem');
+});
