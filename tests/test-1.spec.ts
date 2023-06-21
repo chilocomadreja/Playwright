1fdac8fabb5738167b045fec546040e4f64348fc
chilocomadreja <sebastian.marzec@poczta.fm>
2023-06-21 12:40:20 +0200

    dodanie biblioteki lighthouse

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://test.salesforce.com/');
-  getByRole('link', { name: 'Potencjalni klienci' })
-});
