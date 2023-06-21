01fdb333116ec19307d952acd6f0e9a1f031cc74
chilocomadreja <sebastian.marzec@poczta.fm>
2023-06-21 14:20:58 +0200

    SW-885 gotowy cały

---
+++ tests/SW-837/SW-885.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Napisanie wiadomości w Chatter', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await page.click('text=Log In to Sandbox');
+    await page.type('#username', 'ekspansjanormal@clorce.com.sebamaboxa');
+    await page.type('#password', 'Clorce2@');
+    await page.click('text=Log In to Sandbox');
+  });
+
+  test('Napisanie wiadomości', async ({ page }) => {
+    await page.getByRole('link', { name: 'Potencjalni klienci' }).click();
+    await page.waitForTimeout(5000);
+    await page.getByRole('link', { name: 'Janusz Lech' }).click();
+    await page.waitForTimeout(5000);
+
+    await page.getByRole('tab', { name: 'Chatter' }).click();
+
+    await page.getByTitle('Wpisz tekst...').click();
+    await page.locator('.ql-editor').fill('Nowy tekst do weryfikacji ążźćóęł');
+    await page.getByTitle('Kliknij lub naciśnij klawisze Ctrl i Enter').click();
+
+    const Opis = 'Nowy tekst do weryfikacji ążźćóęł';
+    expect(Opis).toContain('Nowy tekst do weryfikacji ążźćóęł');
+  }); 
+});
