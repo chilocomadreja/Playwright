96a68eaab65eb7faed368302ebda1eaa39d3d046
chilocomadreja <sebastian.marzec@poczta.fm>
2023-06-27 12:53:41 +0200

    SW-892 cały gotowy

--- tests/magda.spec.ts
+++ tests/magda.spec.ts
@@ -34,97 +34,97 @@ test('formlead', async ({ page }) => {
   await page.getByRole('combobox', { name: 'Typ sklepu, --Brak--' } ).click();
   // await page.waitForTimeout(35000);
   await page.getByText('Corporate').click();
-  // await page.getByRole('combobox', { name: 'Typ sklepu, --Brak--' }).click();
-  // // await page.getByText(testData.storeType).click();
-  // await page
-  //   .getByRole('option', { name: testData.storeType, exact: true })
-  //   .locator('span')
-  //   .nth(1)
-  //   .click();
-  // await page.getByRole('combobox', { name: 'Format sklepu, --Brak--' }).click();
-  // await page
-  //   .getByRole('option', { name: testData.storeFormat, exact: true })
-  //   .locator('span')
-  //   .nth(1)
-  //   .click();
-  // await page
-  //   .getByRole('combobox', { name: 'Typ potencjalnego klienta, --Brak--' })
-  //   .click();
-  // await page
-  //   .getByRole('option', { name: testData.clientType, exact: true })
-  //   .locator('span')
-  //   .nth(1)
-  //   .click();
-  // await page
-  //   .getByRole('combobox', { name: 'Powód odrzucenia, --Brak--' })
-  //   .click();
-  // await page
-  //   .getByRole('option', { name: testData.reasonForRejection, exact: true })
-  //   .locator('span')
-  //   .nth(2)
-  //   .click();
-  // await page
-  //   .getByRole('combobox', { name: 'Magazyn logistyczny, --Brak--' })
-  //   .click();
-  // await page
-  //   .getByRole('option', { name: testData.warehouse, exact: true })
-  //   .locator('span')
-  //   .nth(1)
-  //   .click();
-  // await page
-  //   .locator('input[name="Non_disclosure_agreement__c"]')
-  //   .fill(testData.trustAgreement);
-  // await page
-  //   .getByRole('combobox', { name: 'Stan potencjalnego klienta, Nowy' })
-  //   .click();
-  // await page
-  //   .getByRole('option', { name: testData.potentialClient, exact: true })
-  //   .locator('span')
-  //   .nth(2)
-  //   .click();
-  // await page.locator('input[name="MobilePhone"]').fill(testData.mobilePhone);
-  // await page.locator('input[name="Phone"]').fill(testData.phoneNumber);
-  // await page.locator('input[name="Email"]').fill(testData.email);
-  // await page.locator('input[name="REGON__c"]').fill(testData.regon);
-  // await page.locator('input[name="KRS__c"]').fill(testData.krs);
-  // await page.locator('input[name="NIP__c"]').fill(testData.nip);
-  // await page
-  //   .locator(
-  //     '[data-target-selection-name="sfdc:RecordField.Lead.Comment__c"] textarea'
-  //   )
-  //   .fill(testData.comment);
-  // // contact1
-  // await page
-  //   .locator(
-  //     '[data-target-selection-name="sfdc:RecordField.Lead.First_Name_2__c"] textarea'
-  //   )
-  //   .fill(testData.firstContactName);
-  // await page
-  //   .locator(
-  //     '[data-target-selection-name="sfdc:RecordField.Lead.Last_Name_2__c"] textarea'
-  //   )
-  //   .fill(testData.firstContactSurname);
-  // await page
-  //   .locator('input[name="Phone_2__c"]')
-  //   .fill(testData.firstContactPhone);
-  // await page
-  //   .locator('input[name="Email_2__c"]')
-  //   .fill(testData.firstContactEmail);
-  // // contact2
-  // await page
-  //   .locator(
-  //     '[data-target-selection-name="sfdc:RecordField.Lead.First_Name_3__c"] textarea'
-  //   )
-  //   .fill(testData.secondContactName);
-  // await page
-  //   .locator(
-  //     '[data-target-selection-name="sfdc:RecordField.Lead.Last_Name_3__c"] textarea'
-  //   )
-  //   .fill(testData.secondContactSurname);
-  // await page
-  //   .locator('input[name="Phone_3__c"]')
-  //   .fill(testData.secondContactPhone);
-  // await page
+  await page.getByRole('combobox', { name: 'Typ sklepu, --Brak--' }).click();
+  // await page.getByText(testData.storeType).click();
+  await page
+    .getByRole('option', { name: testData.storeType, exact: true })
+    .locator('span')
+    .nth(1)
+    .click();
+  await page.getByRole('combobox', { name: 'Format sklepu, --Brak--' }).click();
+  await page
+    .getByRole('option', { name: testData.storeFormat, exact: true })
+    .locator('span')
+    .nth(1)
+    .click();
+  await page
+    .getByRole('combobox', { name: 'Typ potencjalnego klienta, --Brak--' })
+    .click();
+  await page
+    .getByRole('option', { name: testData.clientType, exact: true })
+    .locator('span')
+    .nth(1)
+    .click();
+  await page
+    .getByRole('combobox', { name: 'Powód odrzucenia, --Brak--' })
+    .click();
+  await page
+    .getByRole('option', { name: testData.reasonForRejection, exact: true })
+    .locator('span')
+    .nth(2)
+    .click();
+  await page
+    .getByRole('combobox', { name: 'Magazyn logistyczny, --Brak--' })
+    .click();
+  await page
+    .getByRole('option', { name: testData.warehouse, exact: true })
+    .locator('span')
+    .nth(1)
+    .click();
+  await page
+    .locator('input[name="Non_disclosure_agreement__c"]')
+    .fill(testData.trustAgreement);
+  await page
+    .getByRole('combobox', { name: 'Stan potencjalnego klienta, Nowy' })
+    .click();
+  await page
+    .getByRole('option', { name: testData.potentialClient, exact: true })
+    .locator('span')
+    .nth(2)
+    .click();
+  await page.locator('input[name="MobilePhone"]').fill(testData.mobilePhone);
+  await page.locator('input[name="Phone"]').fill(testData.phoneNumber);
+  await page.locator('input[name="Email"]').fill(testData.email);
+  await page.locator('input[name="REGON__c"]').fill(testData.regon);
+  await page.locator('input[name="KRS__c"]').fill(testData.krs);
+  await page.locator('input[name="NIP__c"]').fill(testData.nip);
+  await page
+    .locator(
+      '[data-target-selection-name="sfdc:RecordField.Lead.Comment__c"] textarea'
+    )
+    .fill(testData.comment);
+  // contact1
+  await page
+    .locator(
+      '[data-target-selection-name="sfdc:RecordField.Lead.First_Name_2__c"] textarea'
+    )
+    .fill(testData.firstContactName);
+  await page
+    .locator(
+      '[data-target-selection-name="sfdc:RecordField.Lead.Last_Name_2__c"] textarea'
+    )
+    .fill(testData.firstContactSurname);
+  await page
+    .locator('input[name="Phone_2__c"]')
+    .fill(testData.firstContactPhone);
+  await page
+    .locator('input[name="Email_2__c"]')
+    .fill(testData.firstContactEmail);
+  // contact2
+  await page
+    .locator(
+      '[data-target-selection-name="sfdc:RecordField.Lead.First_Name_3__c"] textarea'
+    )
+    .fill(testData.secondContactName);
+  await page
+    .locator(
+      '[data-target-selection-name="sfdc:RecordField.Lead.Last_Name_3__c"] textarea'
+    )
+    .fill(testData.secondContactSurname);
+  await page
+    .locator('input[name="Phone_3__c"]')
+    .fill(testData.secondContactPhone);
+  await page
   
 });
 });
